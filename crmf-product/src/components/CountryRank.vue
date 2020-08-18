811c55c6707851b963dc598ad1d83c7adef4fb8e
santooooooo <santo.shunsuke@gmail.com>
2020-08-19 00:34:12 +0900

    added components

---
+++ crmf-product/src/components/CountryRank.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div v-for="country in CountryData.Countries" v-bind:key="country.Country">
+		{{ country.Country}}
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	props: {
+		CountryData: {
+			type: Object
+		}
+	},
+	mounted () {
+		console.log(this.CountryData)
+	}
+})
+
+</script>
+
+<style>
+</style>
--- crmf-product/src/components/WorldRecord.vue
+++ crmf-product/src/components/WorldRecord.vue
@@ -2,18 +2,55 @@
 	<div class="records">
 		<h1>title</h1>
 		<p>This is world record section.</p>
+		<CountryRank v-bind:country-data="CountryData" />
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import CountryRank from './CountryRank.vue'
 
 export default Vue.extend({
+data: function () {
+	return {
+		globalData: {},
+		CountryData: {}
+	}
+},
+components: {
+	CountryRank
+},
 mounted: function () {
+		interface GlobalData {
+			NewConfirmed: number;
+			TotalConfirmed: number;
+			NewDeaths: number;
+			TotalDeaths: number;
+			NewRecovered: number;
+			TotalRecovered: number;
+		}
+		interface CountryData {
+			Countries: Array<string>;
+		}
 		this.axios.get('https://api.covid19api.com/summary')
 		.then((response) => {
-			const data = response.data
-			console.log(data)
+			const mainData: GlobalData = {
+			NewConfirmed: response.data.Global.NewConfirmed,
+			TotalConfirmed: response.data.Global.TotalConfirmed,
+			NewDeaths: response.data.Global.NewDeaths,
+			TotalDeaths: response.data.Global.TotalDeaths,
+			NewRecovered: response.data.Global.NewRecovered,
+			TotalRecovered: response.data.Global.TotalRecovered
+			}
+
+			this.globalData = mainData
+			console.log(this.globalData)
+
+			const propsData: CountryData = {
+				Countries: response.data.Countries
+			}
+			this.CountryData = propsData
+			console.log(this.CountryData)
 		})
 		.catch((e) => {
 			console.log(e)

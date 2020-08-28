983b4d58814a7725d4a816c666b9b8b9ca2b9b8b
santooooooo <santo.shunsuke@gmail.com>
2020-08-28 19:22:14 +0900

    made func of searching country's name

--- crmf-product/src/components/SearchData.vue
+++ crmf-product/src/components/SearchData.vue
@@ -1,5 +1,11 @@
 <template>
-	<div>
+	<div class="search">
+		<h3>Country's Name</h3>
+		<input type="text" v-model="keyword" @keyup.enter ="result">
+
+		<div v-if="resultData.length != 0">
+			{{ resultData }}
+		</div>
 	</div>
 </template>
 
@@ -7,5 +13,35 @@
 import Vue from 'vue'
 
 export default Vue.extend({
+	data () {
+		return {
+			countryData: [],
+			keyword: null,
+			resultData: []
+		}
+	},
+	mounted () {
+		this.axios.get('https://api.covid19api.com/countries')
+		.then((response) => {
+			this.countryData = response.data
+		})
+		.catch((e) => {
+			console.log(e)
+		})
+	},
+	methods: {
+		result (): void {
+			this.resultData = []
+			let c: number = 0
+			for (let i = 0; i < this.countryData.length; i++) {
+				const country: String = this.countryData[i].Country.toLowerCase()
+				const key: String = this.keyword.toLowerCase()
+				if (country.indexOf(key) > -1) {
+					this.resultData[c] = this.countryData[i].Country
+					c++
+				}
+			}
+		}
+	}
 })
 </script>
